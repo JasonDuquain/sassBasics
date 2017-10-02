202760293836bfd272f9b936c006a192d61ae000
jasonduquain <jduquain@gmail.com>
2017-10-02 08:40:50 -0400

    starting point with package.json and gulpfile and gitignoregnore

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass'); 
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+//var breakpoint = require('breakpoint-sass');
+
+
+
+
