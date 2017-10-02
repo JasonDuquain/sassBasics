2107332e6e4472060a4f1bdc39ed676ac1963995
jasonduquain <jduquain@gmail.com>
2017-10-02 09:50:40 -0400

    finished sect2 styles and updated gulpfile tos

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,19 @@ var sourcemaps = require('gulp-sourcemaps');
 //var breakpoint = require('breakpoint-sass');
 
 
+gulp.task('styles', function() {
+    gulp.src('scss/**/*scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'expanded'
+    })).on('error', sass.logError)
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'));
+});
 
+gulp.task('watch', ['styles'], function() {
+    gulp.watch('scss/**/*scss', ['styles']);
+});
 
+gulp.task('default', ['watch']);
